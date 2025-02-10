8cca900eea884277f29eb382356a772225b28c8b
Diogo Petroski <diogopetroski@gmail.com>
2025-02-10 20:00:33 -0300

    criação do projeto

---
+++ flashpay/src/main/java/com/example/flashpay/FlashpayApplication.java
@@ -0,0 +1,13 @@
+package com.example.flashpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashpayApplication.class, args);
+	}
+
+}
---
+++ flashpay/src/test/java/com/example/flashpay/FlashpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flashpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
